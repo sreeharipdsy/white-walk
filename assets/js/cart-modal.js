a4643e4a12ba3744ba655d4091f587f4c8b2deb6
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-07-31 20:18:27 +0530

    shop-page-update || 9.8.6

--- assets/js/cart-modal.js
+++ assets/js/cart-modal.js
@@ -1,5 +1,17 @@
 
+// function toggleCartModal() {
+//     const modal = document.getElementById('cart-modal');
+//     modal.style.display = modal.style.display === 'block' ? 'none' : 'block';
+// }
 function toggleCartModal() {
-    const modal = document.getElementById('cart-modal');
-    modal.style.display = modal.style.display === 'block' ? 'none' : 'block';
+  const modal = document.getElementById('cart-modal');
+  if (modal.style.display === 'block') {
+    modal.classList.remove('show');
+    modal.classList.add('hide');
+    setTimeout(() => modal.style.display = 'none', 100); // Wait for the animation to finish
+  } else {
+    modal.classList.remove('hide');
+    modal.classList.add('show');
+    modal.style.display = 'block';
+  }
 }
