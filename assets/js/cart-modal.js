d66ad31328c2b4e8375fea4c05fc6fe04550cd0f
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-07-31 09:18:42 +0530

    general-update || 9.3.5

--- assets/js/cart-modal.js
+++ assets/js/cart-modal.js
@@ -1,17 +1,22 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    const cartModal = document.getElementById('cart-modal');
-    const closeModalButton = document.querySelector('.modal .close');
-    const cartIcon = document.getElementById('cart-icon');
-    cartIcon.addEventListener('click', () => {
-        cartModal.style.display = 'block';
-    });
-    closeModalButton.addEventListener('click', () => {
-        cartModal.style.display = 'none';
-    });
-    window.addEventListener('click', (event) => {
-        if (event.target === cartModal) {
-            cartModal.style.display = 'none';
-        }
-    });
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const cartModal = document.getElementById('cart-modal');
+//     const closeModalButton = document.querySelector('.modal .close');
+//     const cartIcon = document.getElementById('cart-icon');
+//     cartIcon.addEventListener('click', () => {
+//         cartModal.style.display = 'block';
+//     });
+//     closeModalButton.addEventListener('click', () => {
+//         cartModal.style.display = 'none';
+//     });
+//     window.addEventListener('click', (event) => {
+//         if (event.target === cartModal) {
+//             cartModal.style.display = 'none';
+//         }
+//     });
+// });
+
+function toggleCartModal() {
+    const modal = document.getElementById('cart-modal');
+    modal.style.display = modal.style.display === 'block' ? 'none' : 'block';
+}
