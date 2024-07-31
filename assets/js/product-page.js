de6b4611a25da5dd5619bbad30fcb0b0a48c68ce
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-07-31 11:08:53 +0530

    shop-page-update || 9.7

--- assets/js/product-page.js
+++ assets/js/product-page.js
@@ -8,5 +8,6 @@ function openProductPage(product) {
     document.getElementById('breadcrumb-product-name').textContent = product.name;
     document.getElementById('product-image').src = product.image;
     document.getElementById('product-name').textContent = product.name;
-    document.getElementById('product-price').innerHTML = `<strike>$${product.price}</strike> $${product.discountedPrice}`;
+    document.getElementById('p-original-price').innerHTML = `₹${product.price}`;
+    document.getElementById('p-discount-price').innerHTML = `₹${product.discountedPrice}`;
 }
