17007752c058555c3451fb288a14ebaaa1da5e3e
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-07-31 09:23:38 +0530

    shop-page-update || 9.3.6

---
+++ assets/js/products.js
@@ -0,0 +1,43 @@
+// PRODUCT LISTS
+const products = [
+    { 
+        id: 1, 
+        name: 'Product 1', 
+        category: 'men', 
+        image: 'image1.jpg', 
+        price: 100, 
+        discountedPrice: 80 
+    },
+    { 
+        id: 2, 
+        name: 'Product 2', 
+        category: 'women', 
+        image: 'image2.jpg', 
+        price: 120, 
+        discountedPrice: 90 
+    },
+    { 
+        id: 3, 
+        name: 'Product 3', 
+        category: 'sports', 
+        image: 'image3.jpg', 
+        price: 150, 
+        discountedPrice: 130
+    },
+    { 
+        id: 4, 
+        name: 'Product 4', 
+        category: 'uaquality', 
+        image: 'image4.jpg', 
+        price: 150, 
+        discountedPrice: 130
+    },
+    { 
+        id: 5, 
+        name: 'Product 5', 
+        category: 'buy1get1', 
+        image: 'image5.jpg', 
+        price: 150, 
+        discountedPrice: 130
+    }
+];
