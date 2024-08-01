7b9bc26a1c2ced0b65f0277a0d6f66b70021bad5
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-08-01 12:48:31 +0530

    trending-section-update || 9.9.2

--- assets/js/shop-trending.js
+++ assets/js/shop-trending.js
@@ -1,36 +1,29 @@
 
 
 function renderTrendingProducts(trendingProducts) {
-  const trendingProductList = document.getElementById('product-list');
-  trendingProductList.innerHTML = '';
-  trendingProducts.forEach(trendingProduct => {
-    const trendingProductCard = document.createElement('div');
-    trendingProductCard.classList.add('card');
-    trendingProductCard.setAttribute('data-aos', 'zoom-in');
-    trendingProductCard.innerHTML = `
-      <div class="product-img">
-        <img 
-          src="${trendingProduct.image}" 
-          class="w-100" 
-          alt="${trendingProduct.name} Image"
-          onclick="openProductPage(${trendingProduct.id})">
-      </div>
-      <div class="product-description">
-        <div class="name-rating">
-          <p class="p-name mb-0">${trendingProduct.name}</p>
-          <div class="p-rating">5<span>★</span></div>
-        </div>
-        <div class="p-price">
-          <p><span>₹${trendingProduct.price}</span> ₹${trendingProduct.discountedPrice}</p>
-        </div>
-        <div class="card-buttons">
-          <button class="add-to-cart" onclick="addToCart(${trendingProduct.id})">Add to Cart</button>
-          <button class="buy-now" onclick="buyNow('${trendingProduct.name}')">Buy Now</button>
-        </div>
-      </div>
+    const trendingProductList = document.getElementById('trending-list');
+    trendingProductList.innerHTML = '';
+    trendingProducts.forEach(trendingProduct => {
+        const trendingProductCard = document.createElement('div');
+        trendingProductCard.classList.add('card');
+        trendingProductCard.setAttribute('data-aos', 'zoom-in');
+        trendingProductCard.innerHTML = `
+            <img 
+                class="w-100"
+                src="${trendingProduct.name}" 
+                alt="${trendingProduct.name} Image" 
+                onclick="openProductPage(${trendingProduct.id})">
+            <h3>product name <span>5 ★</span></h3>
+            <div class="p-price">
+                <p><strike>₹${trendingProduct.price}</strike> ₹${trendingProduct.discountedPrice}</p>
+            </div>
+            <a  class="buy-button text-center"
+                onclick="buyNow('${trendingProduct.name}')">
+                    Enquire Now
+            </a>
     `;
-    // productCard.addEventListener('click', () => openProductPage(product));
-    trendingProductList.appendChild(trendingProductCard);
-  });
+        // productCard.addEventListener('click', () => openProductPage(product));
+        trendingProductList.appendChild(trendingProductCard);
+    });
 }
 
