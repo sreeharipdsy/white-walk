f280fcee1f85ccd4e8600e747053068d1c49a932
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-08-01 14:39:29 +0530

    trending-section-update || 9.9.7

--- assets/js/shop-trending.js
+++ assets/js/shop-trending.js
@@ -17,9 +17,10 @@ function renderTrendingProducts(trendingProducts) {
                 src="${trendingProduct.image}" 
                 alt="${trendingProduct.name} Image" 
                 onclick="openProductPage(${trendingProduct.id})">
-            <h3>${trendingProduct.name}<span>5 ★</span></h3>
+            <h3>${trendingProduct.name}</h3>
             <div class="p-price">
                 <p><strike>₹${trendingProduct.price}</strike> ₹${trendingProduct.discountedPrice}</p>
+                <span>5★</span>
             </div>
             <a  class="buy-button text-center"
                 onclick="buyNow('${trendingProduct.name}')">
