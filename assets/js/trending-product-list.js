8a5372e28cee582b42ff0560c8b4c8d562f04dfe
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-08-01 10:51:21 +0530

    trending-section-update || 9.9

---
+++ assets/js/trending-product-list.js
@@ -0,0 +1,34 @@
+const trendingProducts = [
+    { 
+        id: 1, 
+        name: 'Nike Air Motion Max', 
+        category: 'men', 
+        image: 'assets/images/2.jpeg', 
+        price: 2499, 
+        discountedPrice: 1999
+    },
+    { 
+        id: 2, 
+        name: 'Product 2', 
+        category: 'women', 
+        image: 'assets/images/1.jpeg', 
+        price: 120, 
+        discountedPrice: 90 
+    },
+    { 
+        id: 3, 
+        name: 'Product 3', 
+        category: 'sports', 
+        image: 'assets/images/2.jpeg', 
+        price: 150, 
+        discountedPrice: 130
+    },
+    { 
+        id: 4, 
+        name: 'Product 4', 
+        category: 'uaquality', 
+        image: 'assets/images/2.jpeg', 
+        price: 150, 
+        discountedPrice: 130
+    }
+]
