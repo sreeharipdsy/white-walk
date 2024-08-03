b3c67782cb40ad944b2aff79583f0daed44872f0
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-08-03 06:59:32 +0530

    product-update || 9.13.8

--- products-list.js
+++ products-list.js
@@ -4,7 +4,7 @@ const products = [
         id: 1, 
         name: 'NIKE SB DUNK STUSSY JUST DO IT', 
         category: 'men', 
-        image: 'shop/NIKE SB DUNK STUSSY JUST DO IT.jpeg', 
+        image: 'shop/NIKE SB DUNK STUSSY JUST DO IT.jpg', 
         price: 2999, 
         discountedPrice: 2599,
         size: '40-45'
@@ -13,7 +13,7 @@ const products = [
         id: 2, 
         name: 'NEW BALANCE 530 GREEN', 
         category: 'unisex', 
-        image: 'shop/NEW BALANCE 530 GREEN.jpeg', 
+        image: 'shop/NEW BALANCE 530 GREEN.jpg', 
         price: 2899, 
         discountedPrice: 2575,
         size: '36-44'
@@ -22,7 +22,7 @@ const products = [
         id: 3, 
         name: 'NEW BALANCE 1906 KHAKHI', 
         category: 'men', 
-        image: 'shop/NEW BALANCE 1906 KHAKHI.jpeg', 
+        image: 'shop/NEW BALANCE 1906 KHAKHI.jpg', 
         price: 3499, 
         discountedPrice: 2999,
         size: '40-45'
@@ -31,7 +31,7 @@ const products = [
         id: 4, 
         name: 'ADIDAS FORUM 84 TRIPPLE WHITE', 
         category: 'women', 
-        image: 'shop/ADIDAS FORUM 84 TRIPPLE WHITE.jpeg', 
+        image: 'shop/ADIDAS FORUM 84 TRIPPLE WHITE.jpg', 
         price: 3499, 
         discountedPrice: 2799,
         size: '36-40'
@@ -40,9 +40,64 @@ const products = [
         id: 5, 
         name: 'NIKE SB DUNK MOCHA', 
         category: 'men', 
-        image: 'shop/NIKE SB DUNK MOCHA.jpeg', 
+        image: 'shop/NIKE SB DUNK MOCHA.jpg', 
         price: 2999, 
         discountedPrice: 2499,
         size: '40-45'
+    },
+    {
+        id: 6,
+        name: 'Nike Air Jordan 6 British khakhi',
+        category: 'uaquality',
+        image: 'shop/Nike Air Jordan 6 British khakhi.jpg',
+        price: 4299,
+        discountedPrice: 3599,
+        size: '41-46'
+    },
+    {
+        id: 7,
+        name: 'NIKE SB DUNK KLEKT',
+        category: 'men',
+        image: 'shop/NIKE SB DUNK KLEKT.jpg',
+        price: 3399,
+        discountedPrice: 2699,
+        size: '40-45'
+    },
+    {
+        id: 8,
+        name: 'Nike Sb Dunk Low x Otomo Katsuhiro green',
+        category: 'men',
+        image: 'shop/Nike Sb Dunk Low x Otomo Katsuhiro green.jpg',
+        price: 2999,
+        discountedPrice: 2499,
+        size: '40-45'
+    },
+    {
+        id: 9,
+        name: 'ADDIDAS ORGINALS CAMPUS DARK GREEN',
+        category: 'unisex',
+        image: 'shop/ADDIDAS ORGINALS CAMPUS DARK GREEN.jpg',
+        price: 3299,
+        discountedPrice: 2599,
+        size: '36-44'
+    },
+    {
+        id: 10,
+        name: 'NEW BALANCE 1906 TRIPPLE BLACK',
+        category: 'men',
+        image: 'shop/NEW BALANCE 1906 TRIPPLE BLACK.jpg',
+        price: 3599,
+        discountedPrice: 2975,
+        size: '40-45'
     }
+    // ,
+    // {
+    //     id: ,
+    //     name: '',
+    //     category: '',
+    //     image: 'shop/',
+    //     price: ,
+    //     discountedPrice: ,
+    //     size: ''
+    // }
 ];
